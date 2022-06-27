678f7db3e92af9b5e913338d14e0770245c4d908
Zoey <z@zoey.ninja>
2022-06-27 17:53:53 -0500

    added navbar and themes

---
+++ components/Navbar.tsx
@@ -0,0 +1,67 @@
+const availableThemes = [
+  "light",
+  "dark",
+  "cupcake",
+  //   "bumblebee",
+  //   "emerald",
+  //   "corporate",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "valentine",
+  "halloween",
+//   "garden",
+//   "forest",
+//   "aqua",
+//   "lofi",
+//   "pastel",
+//   "fantasy",
+//   "wireframe",
+//   "black",
+//   "luxury",
+//   "dracula",
+//   "cmyk",
+//   "autumn",
+//   "business",
+//   "acid",
+//   "lemonade",
+//   "night",
+//   "coffee",
+//   "winter",
+];
+
+type NavbarProps = {
+  setTheme: (value: string) => void;
+};
+
+const Navbar = ({ setTheme }: NavbarProps) => (
+  <div className="navbar bg-base-100">
+    <div className="flex-1">
+      <a className="btn btn-ghost normal-case text-xl">title</a>
+    </div>
+
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn m-1">
+        Click
+      </label>
+
+      <ul tabIndex={0} className="dropdown-content menu p-2 w-52">
+        {availableThemes.map((themeName) => (
+          <li key={themeName}>
+            <div data-theme={themeName} className="m-1 p-0">
+              <div
+                className="btn btn-outline btn-block p-1"
+                onClick={() => setTheme(themeName)}
+                data-theme={themeName}
+              >
+                {themeName}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
+
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar';
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [theme, setTheme] = useState('light')
+
+  return (
+    <div data-theme={theme}>
+      <Navbar setTheme={setTheme} />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
